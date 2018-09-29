5e9acaf69230b884a38e03bb1a8d137fd10eb53e
francisco-chavez <francisco.chavez.509@outlook.com>
2018-09-29 03:01:42 -0700

    Made some changes to Shape.
    I wanted to increase performance of setting the shape values with the index operator, but that lead to the discover of a few issues. An call to an empty constructor will be used for a struct even when the decalred constructor falls within the use of an empty construtor. Since I can't define an empty constructor for a struct, that means that the array containing the shape doens't get created. So, I switched the Shape from a struct to a class. I really wanted it as a shape, but I want good performance indexing more. Besides, with a struct, the rows and columns would have been initialzed to zero on the default constructor, and that's something I really, really don't want at this time.

--- Code/NN Set01 SLN/ConsoleApp01/Shape.cs
+++ Code/NN Set01 SLN/ConsoleApp01/Shape.cs
@@ -1,58 +1,63 @@
 ﻿
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace ConsoleApp01
 {
-	public struct Shape
+	public class Shape
 	{
 
 		#region Properties
 
 		public uint Rows
 		{
-			get { return _rows; }
+			get { return _shape[0]; }
 			set
 			{
 				if (value < 1)
 					throw new ArgumentOutOfRangeException();
 
-				_rows = value;
+				_shape[0] = value;
 			}
 		}
-		private uint _rows;
 
 		public uint Columns
 		{
-			get { return _columns; }
+			get { return _shape[1]; }
 			set
 			{
 				if (value < 1)
 					throw new ArgumentOutOfRangeException();
 
-				_columns = value;
+				_shape[1] = value;
 			}
 		}
-		private uint _columns;
+
+		private uint[] _shape;
 
 		#endregion
 
 
 		#region Constructors
 
+		public Shape() : this(1, 1) { }
+
 		public Shape(uint rows = 1, uint columns = 1)
 		{
+		
 			if (rows < 1)
 				throw new ArgumentOutOfRangeException("rows");
 			if (columns < 1)
 				throw new ArgumentOutOfRangeException("columns");
 
-			_rows		= rows;
-			_columns	= columns;
+			_shape = new uint[] { rows, columns };
+		}
+
+		public Shape(Shape other)
+		{
+			_shape = new uint[other._shape.Length];
+
+			Array.Copy(other._shape, _shape, _shape.Length);
 		}
 
 		#endregion
@@ -62,24 +67,13 @@ namespace ConsoleApp01
 
 		public uint this[int key]
 		{
-			get
-			{
-				if (key < 0 || key > 1)
-					throw new IndexOutOfRangeException();
-
-				return (key == 0) ? _rows : _columns;
-			}
+			get { return _shape[key]; }
 			set
 			{
 				if (value < 1)
 					throw new ArgumentOutOfRangeException();
 
-				if (key == 0)
-					_rows = value;
-				else if (key == 1)
-					_columns = value;
-				else
-					throw new IndexOutOfRangeException();
+				_shape[key] = value;
 			}
 		}
 
@@ -89,9 +83,6 @@ namespace ConsoleApp01
 		}
 		public static implicit operator Shape(Tuple<int, int> shape)
 		{
-			if (shape.Item1 < 0 || shape.Item2 < 0)
-				throw new ArgumentOutOfRangeException("Shape values must be non-negative.");
-
 			return new Shape((uint) shape.Item1, (uint) shape.Item2);
 		}
 		public static implicit operator Shape(uint[] shape)
@@ -101,14 +92,9 @@ namespace ConsoleApp01
 
 			var result = new Shape();
 
-			for (int i = 0; i < 2; i++)
+			for (int i = 0; i < result._shape.Length; i++)
 				if (shape.Length > i)
-				{
-					if (shape[i] < 1)
-						throw new ArgumentOutOfRangeException();
-
 					result[i] = shape[i];
-				}
 
 			return result;
 		}
@@ -119,14 +105,9 @@ namespace ConsoleApp01
 
 			var result = new Shape();
 
-			for (int i = 0; i < 2; i++)
+			for (int i = 0; i < result._shape.Length; i++)
 				if (shape.Length > i)
-				{
-					if (shape[i] < 1)
-						throw new ArgumentOutOfRangeException();
-
 					result[i] = (uint) shape[i];
-				}
 
 			return result;
 		}
