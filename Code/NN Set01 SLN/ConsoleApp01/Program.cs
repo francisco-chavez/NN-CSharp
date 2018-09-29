4f3eabacb4a5d2a28712327471ffa4fb9627d2a5
francisco-chavez <francisco.chavez.509@outlook.com>
2018-09-29 02:29:20 -0700

    Added First Project
    I'm going to want to turn this into a Library. To figure out how to best do that, I'm going to have to write a few NNs first, just to see how to best abstract things out. This means, I need to add a very first project. The NN course I'm taking is in Python and uses NumPy, so I've already got started with a Shape struct that covers the needs of the NNs I've studied so far.

---
+++ Code/NN Set01 SLN/ConsoleApp01/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+namespace ConsoleApp01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Shape matrixSize = new Shape { Rows = 3, Columns = 5 };
+		}
+	}
+}
---
+++ Code/NN Set01 SLN/ConsoleApp01/Shape.cs
@@ -0,0 +1,137 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp01
+{
+	public struct Shape
+	{
+
+		#region Properties
+
+		public uint Rows
+		{
+			get { return _rows; }
+			set
+			{
+				if (value < 1)
+					throw new ArgumentOutOfRangeException();
+
+				_rows = value;
+			}
+		}
+		private uint _rows;
+
+		public uint Columns
+		{
+			get { return _columns; }
+			set
+			{
+				if (value < 1)
+					throw new ArgumentOutOfRangeException();
+
+				_columns = value;
+			}
+		}
+		private uint _columns;
+
+		#endregion
+
+
+		#region Constructors
+
+		public Shape(uint rows = 1, uint columns = 1)
+		{
+			if (rows < 1)
+				throw new ArgumentOutOfRangeException("rows");
+			if (columns < 1)
+				throw new ArgumentOutOfRangeException("columns");
+
+			_rows		= rows;
+			_columns	= columns;
+		}
+
+		#endregion
+
+
+		#region Operators
+
+		public uint this[int key]
+		{
+			get
+			{
+				if (key < 0 || key > 1)
+					throw new IndexOutOfRangeException();
+
+				return (key == 0) ? _rows : _columns;
+			}
+			set
+			{
+				if (value < 1)
+					throw new ArgumentOutOfRangeException();
+
+				if (key == 0)
+					_rows = value;
+				else if (key == 1)
+					_columns = value;
+				else
+					throw new IndexOutOfRangeException();
+			}
+		}
+
+		public static implicit operator Shape(Tuple<uint, uint> shape)
+		{
+			return new Shape(shape.Item1, shape.Item2);
+		}
+		public static implicit operator Shape(Tuple<int, int> shape)
+		{
+			if (shape.Item1 < 0 || shape.Item2 < 0)
+				throw new ArgumentOutOfRangeException("Shape values must be non-negative.");
+
+			return new Shape((uint) shape.Item1, (uint) shape.Item2);
+		}
+		public static implicit operator Shape(uint[] shape)
+		{
+			if (shape == null)
+				throw new ArgumentNullException();
+
+			var result = new Shape();
+
+			for (int i = 0; i < 2; i++)
+				if (shape.Length > i)
+				{
+					if (shape[i] < 1)
+						throw new ArgumentOutOfRangeException();
+
+					result[i] = shape[i];
+				}
+
+			return result;
+		}
+		public static implicit operator Shape(int[] shape)
+		{
+			if (shape == null)
+				throw new ArgumentNullException();
+
+			var result = new Shape();
+
+			for (int i = 0; i < 2; i++)
+				if (shape.Length > i)
+				{
+					if (shape[i] < 1)
+						throw new ArgumentOutOfRangeException();
+
+					result[i] = (uint) shape[i];
+				}
+
+			return result;
+		}
+
+		#endregion
+
+	}
+}
